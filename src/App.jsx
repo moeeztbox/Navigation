5de0d6835bf96033ef82968378bbca605279bcb1
moeeztbox <moeez.tbox@gmail.com>
2024-08-20 21:26:25 +0500

    github finder project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
-import './index.css'
 function App() {
-  
-  return <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../Components/users/UserResults";
+import UserSearch from "../Components/users/UserSearch";
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
