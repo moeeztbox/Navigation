9c9621b5a77c81dcc4841cf495d2dcc1552d7b59
moeeztbox <moeez.tbox@gmail.com>
2024-08-21 10:04:58 +0500

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,32 @@ import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
+function App() {
+  const ref = useRef(false);
+  const [count, setcount] = useState(0);
+  const [data, setdata] = useState(0);
 
-function App(){
-  const ref=useRef(false)
-  const [count,setcount]=useState(0);
-const [data,setdata]=useState(0);
+  useEffect(() => {
+    if (ref.current) return;
+    setdata(data + 1);
+    alert("welcome to the page");
+    ref.current = true;
+  }, [count]);
 
-
-useEffect(()=>{
-  if(ref.current)return;
-  setdata(data+1)
-  alert("welcome to the page")
-  ref.current=true
-
-},[count])
-
-
-return(
-  <>
-    <button onClick={()=>setcount(count+1)}>count{count}</button>
-    <button onClick={()=>setdata(data+1)}>update{data}</button>
-    <button onClick={()=>setdata(()=>{setdata(0),setcount(0)})}>reset</button>
-  </>
-)
+  return (
+    <>
+      <button onClick={() => setcount(count + 1)}>count{count}</button>
+      <button onClick={() => setdata(data + 1)}>update{data}</button>
+      <button
+        onClick={() =>
+          setdata(() => {
+            setdata(0), setcount(0);
+          })
+        }
+      >
+        reset
+      </button>
+    </>
+  );
 }
-export default App
+export default App;
